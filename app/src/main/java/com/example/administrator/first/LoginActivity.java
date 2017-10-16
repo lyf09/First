6d6eba9a076cff10b90d9d5bdf85f344511ac61a
lyf09 <2331692234@qq.com>
2017-10-16 09:53:17 +0800

    login

---
+++ app/src/main/java/com/example/administrator/first/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.first;
+
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
