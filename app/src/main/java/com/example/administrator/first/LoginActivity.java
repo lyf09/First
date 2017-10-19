7dd8eca0767c89df0a26b4daa52f619a80f1c3b8
lyf9 <2331692234@qq.com>
2017-10-19 11:45:12 +0800

    main

--- app/src/main/java/com/example/administrator/first/LoginActivity.java
+++ app/src/main/java/com/example/administrator/first/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.administrator.first;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Administrator on 2017/10/16.
@@ -13,7 +16,16 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
+
+        Button button = (Button)findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
